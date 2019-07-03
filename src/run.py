227f980dd67ff1d7f8f1cb404bfa6c9cd02d46fc
Olaf Haag <haag.olaf@gmail.com>
2019-07-03 23:52:04 +0200

    rename folder

--- src/run.py
+++ src/run.py
@@ -1,9 +1,8 @@
 import os
-from ucmwebapp import app, debug
+from src.ucmwebapp import app, debug
 
 server = app.server
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run_server(debug=debug, port=port, threaded=True)
-#    server = app.server
