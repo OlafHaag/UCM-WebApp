124b4f9456a3b68521b2cfae30c89068a00294fa
Olaf Haag <haag.olaf@gmail.com>
2019-07-25 22:11:28 +0200

    broke up parsing

--- app/dashapp1/callbacks.py
+++ app/dashapp1/callbacks.py
@@ -205,14 +205,14 @@ def add_to_db(device_kwargs, user_kwargs, session_kwargs, trials_kwargs):
         sessions.append(session)
     
     # Add trials.
-    # FixMe: Optimize with df.to_sql(name=sql_table_name, con=db.engine, if_exists='append', index=False, method='multi')?
+    # FixMe: Optimize with df.to_sql(name='circle_tasks', con=db.engine, if_exists='append', index=False, method='multi')?
     trials = list()
     for kw in trials_kwargs:
         try:
             session_idx = kw.pop('session_idx')
         except KeyError:
             raise ModelCreationError("ERROR: Failed to relate trial to session.")
-        #FixMe: add session. session isn't nullable -> IntegrityError
+        # Add session relationship.
         kw['session'] = sessions[session_idx].id
         try:
             trial, created = get_one_or_create(CircleTask, create_func=new_circletask_trial, **kw)
@@ -277,24 +277,6 @@ def get_table_idx(filenames, table_type):
     return file_idx
 
 
-def get_idx_dict(list_of_filenames):
-    """ Take list of file names and map table types to the list indices.
-    
-    :param list_of_filenames: List of received file names.
-    :return: Dictionary with indices of uploaded files in list: device, user, session, trials
-    :rtype: dict
-    """
-    indices = dict()
-    # If files are missing get_table_idx raises UploadError.
-    indices['device'] = get_table_idx(list_of_filenames, 'device')
-    indices['user'] = get_table_idx(list_of_filenames, 'user')
-    indices['session'] = get_table_idx(list_of_filenames, 'session')
-    indices['trials'] = get_file_indices(list_of_filenames, 'trials')
-    if not indices['trials']:
-        raise UploadError("ERROR: Trial files are missing.")
-    return indices
-
-
 #########################
 # Extract file contents #
 #########################
@@ -311,23 +293,6 @@ def decode_contents(list_of_contents):
         content_type, content_string = contents.split(',')
         decoded.append(base64.b64decode(content_string).decode('utf-8'))
     return decoded
-
-
-def content_to_df(content):
-    """ Decode uploaded file content and return Dataframe.
-
-    :param content: base64 encoded content of a file.
-    :type content: bytes
-    :return: Converted Dataframe
-    :rtype: pandas.DataFrame
-    """
-    decoded = base64.b64decode(content)
-    try:
-        df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
-    except Exception as e:
-        print(e)
-        return None  # ToDo raise Error instead.
-    return df
 
 
 ####################
@@ -385,6 +350,62 @@ def check_circletask_integrity(df, sent_hash, default=10.0):
     return check
 
 
+##################
+# Parsing upload #
+##################
+def get_device_properties(csv_file):
+    """ Get device properties as dictionary from a CSV file.
+    
+    :param csv_file: 1 row table with device properties.
+    :type csv_file: str|io.StringIO
+    :return: Properties of the device.
+    :rtype: dict
+    """
+    try:
+        # For device and user we expect them to contain only 1 entry.
+        props = pd.read_csv(csv_file).iloc[0].to_dict()  # df->Series->dict
+    except Exception:
+        raise UploadError("ERROR: Failed to read file contents for device.")
+    return props
+    
+    
+def get_user_properties(csv_file):
+    """ Get user data as dictionary from a CSV file.
+
+    :param csv_file: 1 row table with user data.
+    :type csv_file: str|io.StringIO
+    :return: Properties of the user.
+    :rtype: dict
+    """
+    try:
+        # For device and user we expect them to contain only 1 entry.
+        props = pd.read_csv(csv_file).iloc[0].to_dict()  # df->Series->dict
+    except Exception:
+        raise UploadError("ERROR: Failed to read file contents for user.")
+    return props
+
+
+def get_session_df(csv_file, user_id):
+    """ Return a DataFrame from CSV file or buffer and add user_id column.
+    
+    :param csv_file: Table with session data.
+    :type csv_file: str|io.StringIO
+    :param user_id: ID of the user who performed the session.
+    :type user_id: str
+    :return: Properties of the blocks.
+    :rtype: pandas.DataFrame
+    """
+    try:
+        # Read data and keep empty string in treatment as empty string, not NaN.
+        session_df = pd.read_csv(csv_file, keep_default_na=False)
+        # Convert time_iso string to datetime.
+        session_df['time_iso'] = session_df['time_iso'].apply(lambda t: datetime.strptime(t, time_fmt))
+        session_df['user_id'] = user_id
+    except Exception:
+        raise UploadError("ERROR: Failed to read file contents for session.")
+    return session_df
+
+    
 def get_trials_meta(filename):
     """ Take a filename and extract time_iso and block information from it.
     
@@ -403,10 +424,65 @@ def get_trials_meta(filename):
     meta = namedtuple('trialMeta', ['time_iso', 'block'])
     return meta(time_iso, block)
     
+
+def get_trials_properties(filenames, contents, times, blocks, hashes, user_id):
+    """
     
-################
-# Parsing main #
-################
+    :param filenames: All uploaded filenames.
+    :type filenames: list
+    :param contents: Decoded file contents.
+    :type contents: list
+    :param times: 'time_iso' column of session.
+    :type times: pandas.Series
+    :param blocks: 'block' column of session.
+    :type blocks: pandas.Series
+    :param hashes: 'hash' column of session.
+    :type hashes: pandas.Series
+    :param user_id: ID of user who provided the data of trials.
+    :type user_id: str
+    :return: Properties of all the trials.
+    :rtype: dict
+    """
+    file_indices = get_file_indices(filenames, 'trials')
+    if not file_indices:
+        raise UploadError("ERROR: Trial files are missing.")
+    
+    trials_dfs = list()
+    for idx in file_indices:
+        # Get information from filename.
+        try:
+            trials_meta = get_trials_meta(filenames[idx])
+        except UploadError:
+            raise
+        # Get data from content.
+        try:
+            df = pd.read_csv(io.StringIO(contents[idx]))
+        except Exception:
+            raise UploadError("ERROR: Failed to read file contents for trials.")
+        # Add index of session for later relationship assignment.
+        mask = (times == trials_meta.time_iso) & (blocks == trials_meta.block)
+        try:
+            session_idx = mask[mask].index[0]
+            df['session_idx'] = session_idx
+        except IndexError:
+            raise UploadError("ERROR: Mismatch between session data and trials.")
+        # Check data integrity.
+        sent_hash = hashes.iloc[session_idx]
+        try:
+            check_passed = check_circletask_integrity(df, sent_hash)
+        except UploadError:
+            raise
+        trials_dfs.append(df)
+    
+    # Concatenate the different trials DataFrames. Rows are augmented by block & time_iso for differentiation later on.
+    df = pd.concat(trials_dfs)
+    df['user_id'] = user_id
+    df['trial'] = df.index
+    # We may get a lot of trials, put them in a generator to not hold a large list of dictionaries in memory.
+    props = (row._asdict() for row in df.itertuples(index=False))
+    return props
+
+
 def parse_uploaded_files(list_of_filenames, list_of_contents):
     """ Reads files and returns dictionary with keyword arguments for each table type.
     These are:
@@ -429,7 +505,10 @@ def parse_uploaded_files(list_of_filenames, list_of_contents):
     
     # If there was an error in mapping the table types to files, return that error.
     try:
-        table_idx = get_idx_dict(list_of_filenames)
+        # If files are missing get_table_idx raises UploadError.
+        device_idx = get_table_idx(list_of_filenames, 'device')
+        user_idx = get_table_idx(list_of_filenames, 'user')
+        session_idx = get_table_idx(list_of_filenames, 'session')
     except UploadError:
         raise
     
@@ -442,62 +521,22 @@ def parse_uploaded_files(list_of_filenames, list_of_contents):
     # Extract data from content for the database models.
     kw = dict()  # Keyword arguments for each table.
     try:
-        # For device and user we expect them to contain only 1 entry.
-        kw['device'] = pd.read_csv(io.StringIO(decoded_list[table_idx['device']])).iloc[0].to_dict()  # df->Series->dict
-    except Exception:
-        raise UploadError("ERROR: Failed to read file contents for device.")
-    try:
-        kw['user'] = pd.read_csv(io.StringIO(decoded_list[table_idx['user']])).iloc[0].to_dict()  # df->Series->dict
-    except Exception:
-        raise UploadError("ERROR: Failed to read file contents for user.")
-    try:
-        # Read data and keep empty string in treatment as empty string, not NaN.
-        session_df = pd.read_csv(io.StringIO(decoded_list[table_idx['session']]), keep_default_na=False)
-        # Convert time_iso string to datetime.
-        session_df['time_iso'] = session_df['time_iso'].apply(lambda t: datetime.strptime(t, time_fmt))
-        session_df['user_id'] = kw['user']['id']
-    except Exception:
-        raise UploadError("ERROR: Failed to read file contents for session.")
-    # We expect only a few blocks for a session, so put it all in one list.
-    kw['session'] = [data._asdict() for data in session_df.itertuples(index=False)]  # Convert namedtuple to dict.
-    
-    trials_dfs = list()
-    for idx in table_idx['trials']:
-        # Get information from filename.
-        try:
-            trials_meta = get_trials_meta(list_of_filenames[idx])
-        except UploadError:
-            raise
-        # Get data from content.
-        try:
-            df = pd.read_csv(io.StringIO(decoded_list[idx]))
-        except Exception:
-            raise UploadError("ERROR: Failed to read file contents for trials.")
-        # Add index of session for later relationship assignment.
-        mask = (session_df['time_iso'] == trials_meta.time_iso) & (session_df['block'] == trials_meta.block)
-        try:
-            session_idx = session_df[mask].index[0]
-            df['session_idx'] = session_idx
-        except IndexError:
-            raise UploadError("ERROR: Mismatch between session data and trials.")
-        # Check data integrity.
-        sent_hash = session_df['hash'].iloc[session_idx]
-        try:
-            check_passed = check_circletask_integrity(df, sent_hash)
-        except UploadError:
-            raise
-        trials_dfs.append(df)
-        
-    # Concatenate the different trials DataFrames. Rows are augmented by block & time_iso for differentiation later on.
-    df = pd.concat(trials_dfs)
-    df['user_id'] = kw['user']['id']
-    df['trial'] = df.index
-    # We may get a lot of trials, put them in a generator to not hold a large list of dictionaries in memory.
-    kw['trials'] = (row._asdict() for row in df.itertuples(index=False))
+        kw['device'] = get_device_properties(io.StringIO(decoded_list[device_idx]))
+        kw['user'] = get_user_properties(io.StringIO(decoded_list[user_idx]))
+        session_df = get_session_df(io.StringIO(decoded_list[session_idx]), kw['user']['id'])
+        kw['session'] = [data._asdict() for data in session_df.itertuples(index=False)]  # Convert namedtuple to dict.
+        kw['trials'] = get_trials_properties(list_of_filenames,
+                                             decoded_list,
+                                             session_df['time_iso'],
+                                             session_df['block'],
+                                             session_df['hash'],
+                                             kw['user']['id'])
+    except UploadError:
+        raise
     
     return kw
     
-    
+
 def process_upload(filenames, contents):
     """
     
@@ -516,8 +555,11 @@ def process_upload(filenames, contents):
         add_to_db(kw['device'], kw['user'], kw['session'], kw['trials'])
     except ModelCreationError:
         raise
+ 
     
-    
+################
+# Analyze Data #
+################
 def get_db_data():
     pass
 
@@ -570,7 +612,9 @@ def register_callbacks(dashapp):
         """ Handles files that are sent to the dash update component."""
         if list_of_contents is not None:
             try:
+                # Insert data into database.
                 process_upload(list_of_names, list_of_contents)
+                # FixMe: Remove Debug Vizualisation.
                 children = [
                     parse_upload_contents(c, n, d) for c, n, d in zip(list_of_contents, list_of_names, list_of_dates)]
             except (UploadError, ModelCreationError) as e:
