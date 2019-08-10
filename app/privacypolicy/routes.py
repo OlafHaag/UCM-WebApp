140da30e0923ad294c944b7013dc5c0861494395
Olaf Haag <haag.olaf@gmail.com>
2019-08-10 02:17:31 +0200

    better privacy policy

--- app/privacypolicy/routes.py
+++ app/privacypolicy/routes.py
@@ -1,6 +1,17 @@
+import os
+from pathlib import Path
+
 from flask import Blueprint, render_template
 from flask import current_app as app
 
+author = os.environ.get('AUTHOR')
+street = os.environ.get('STREET')
+postal = os.environ.get('POSTAL')
+city = os.environ.get('CITY')
+email = os.environ.get('EMAIL')
+
+templates_path = Path(__file__).parent / "templates"
+information_html = (templates_path / "information.html").read_text(encoding='utf-8').format(name=author, email=email)
 
 # Blueprint Configuration
 privacy_bp = Blueprint('privacy_bp', __name__,
@@ -14,4 +25,4 @@ def privacy():
     return render_template('index.html',
                            title='Privacy Policy',
                            template='privacy-template',
-                           body="All your base are belong to us.")
+                           body=information_html)
