dd5a719a89a4856f3e473b043bb21e7c054a3437
Olaf Haag <haag.olaf@gmail.com>
2020-07-31 21:29:05 +0200

    set logging level to gunicorn env cmd arg

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,5 @@
-"""Application entry point."""
+"""  Application entry point. """
+import logging
 import os
 from pathlib import Path
 from dotenv import load_dotenv
@@ -16,5 +17,14 @@ else:
 app = create_app()
 
 if __name__ == "__main__":
+    # Set Flask application logger’s handlers to the same as gunicorn’s.
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    # Respect gunicorn's --log-level cmd arg.
+    app.logger.setLevel(gunicorn_logger.level)
+    # Silence server routes.
+    #logging.getLogger('werkzeug').setLevel(logging.ERROR)
+    
+    # Run the app.
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=debug, port=port, threaded=True)
